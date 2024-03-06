0c935105236d7b4ede82852fe4bf21f7b213aee2
Assem <asemelbadahy@gmail.com>
2024-03-06 16:42:25 +0200

    [some changes]

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express =require('express');
 const app=express();
 
-app.router("/users",)
+
 app.get("/",(req,res)=>{
     res.send("<h1>Hello world");
 })
 
-app.listen(3000);
+app.listen(3000,()=>console.log("server is running on port 3000"));
