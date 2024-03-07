bb373aa1a83aee14226550741085567b94873b6c
Assem <asemelbadahy@gmail.com>
2024-03-07 18:48:47 +0200

    [update] change user.service name to parent.service

--- controllers/parent.controller.js
+++ controllers/parent.controller.js
@@ -1,7 +1,7 @@
 // userRoutes.js
 import express from 'express';
 const router = express.Router();
-import { getParents } from '../services/user.service.js';
+import { getParents } from '../services/parent.service.js';
 
 router.get('/', (req,res)=>{
     res.send(getParents());
