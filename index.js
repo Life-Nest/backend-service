65be5ce19e1e3a3d1fd4a3244a15da592a3ad180
Assem <asemelbadahy@gmail.com>
2024-03-07 18:41:54 +0200

    [update] adding endpoint

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from 'express';
 const app=express();
 import authRoutes from './controllers/auth.controller.js';
-import userRoutes from './controllers/user.controller.js';
+import userRoutes from './controllers/parent.controller.js';
 
 app.use('/auth',authRoutes);
-app.use('/users',userRoutes);
+app.use('/parent',userRoutes);
 app.listen(3000,()=>console.log("server is running on port 3000"));
-
-
-
