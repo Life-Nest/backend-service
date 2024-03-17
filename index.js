c5120dea1d5058f4eac3a3808c0fa9f656f82400
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-03-17 20:22:22 +0000

    [chore] Update app entry point with incubator controller

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express';
-const app=express();
 import authRoutes from './controllers/user.auth.controller.js';
 import userRoutes from './controllers/user.controller.js';
+import incubatorRoutes from './controllers/incubator.controller.js';
+
+const app = express();
+
 app.use(express.json());
-app.use('/user/auth',authRoutes);
-app.use('/parent',userRoutes);
-app.listen(3000,()=>console.log("server is running on port 3000"));  
+app.use('/user/auth', authRoutes);
+app.use('/parent', userRoutes);
+app.use('/api/v1', incubatorRoutes);
 
-
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+}); 
