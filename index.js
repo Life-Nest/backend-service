c72d8d27eac719949b888bc14e239a795562c95a
Assem <asemelbadahy@gmail.com>
2024-03-17 22:23:31 +0200

    [update] format the code

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from 'express';
-const app=express();
+const app = express();
 import authRoutes from './controllers/user.auth.controller.js';
 import userRoutes from './controllers/user.controller.js';
 app.use(express.json());
-app.use('/user/auth',authRoutes);
-app.use('/user',userRoutes);
-app.listen(3000,()=>console.log("server is running on port 3000"));  
-
- 
+app.use('/user/auth', authRoutes);
+app.use('/user', userRoutes);
+app.listen(3000, () => console.log("server is running on port 3000"));  
