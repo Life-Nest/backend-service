45ee46a34ed140104b6fcfa86f10c9113cc906b8
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-03-25 12:15:21 +0000

    test: Update the testing script to show the response header information

--- api_test.bash
+++ api_test.bash
@@ -7,4 +7,4 @@
 # Example Usage:
 # ./test.bash {REQUEST_METHOD} {REQUESTED_RESOURCE} {REQUEST_PAYLOAD}
 
-curl -s -X "$1" -H "Content-Type: application/json" -d "$3" localhost:3000/api/v1/"$2" | json
+curl -s -i -X "$1" -H "Content-Type: application/json" -d "$3" localhost:3000/"$2" | json
