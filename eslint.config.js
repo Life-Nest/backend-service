4d7e3cd223c51d3d8f8a27ba5c523891638da0b2
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-05-23 09:16:49 +0000

    chore: Update eslint config to bypass a rule causing problems

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,15 @@ import globals from "globals";
 import js from "@eslint/js";
 
 
+const { 'no-useless-assignment': badRule, ...jsRules } = js.configs.all.rules;
+
+
 export default [
-  js.configs.recommended,
+  //js.configs.all,
   {
     files: ['**/*.{js,mjs,cjs}'],
     rules: {
+      ...jsRules,
       'sort-imports': 'off',
       'one-var': 'off',
       'no-console': 'off',
