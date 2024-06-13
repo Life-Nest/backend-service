6ef11278269746fede3086faad0f0de1aa41bbe0
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-06-13 21:53:12 +0000

    feat: Update the script to use an authorization token

--- api_test.bash
+++ api_test.bash
@@ -7,4 +7,4 @@
 # Example Usage:
 # ./api_test.bash {REQUEST_METHOD} {REQUESTED_RESOURCE} {REQUEST_PAYLOAD}
 
-curl -s -i -X "$1" -H "Content-Type: application/json" -d "$3" localhost:3000/"$2" | json
+curl -s -i -X "$1" -H "Content-Type: application/json" -H "Authorization: $4" -d "$3" localhost:3000/"$2" | json
