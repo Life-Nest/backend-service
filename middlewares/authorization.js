6ee0292900b362fd4e268f9c70bf1c2d33e2e6a8
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-06-16 21:58:21 +0000

    feat: Add hospital authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -32,7 +32,36 @@ export function authorizeParent(req, res, next) {
   }
 }
 
+export function authorizeHospital(req, res, next) {
+  const token = req.headers.authorization;
 
+  if (!token) {
+    return res.status(401).json({
+      error: {
+        message: 'Unauthorized',
+        code: 401
+      }
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const requiredRole = 'hospital';
+    if (decoded.role === requiredRole) {
+      req.body.hospitalId = decoded.id;
+      next();
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    res.status(403).json({
+      error: {
+        message: 'Invalid token',
+        code: 403
+      }
+    });
+  }
+}
 
 export function authorizeStaff(req, res, next) {
   const token = req.headers.authorization;
