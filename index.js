2dc52f6afcf0e5372069f49097caba14af631460
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-06-20 14:56:00 +0000

    feat: Add web socket

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { fileURLToPath } from 'url';
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
+import jwt from 'jsonwebtoken';
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -15,8 +18,70 @@ import staffRoutes from './controllers/staff.controller.js';
 dotenv.config();
 
 const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const connectedClients = new Map();
+let id = 0;
+
+wss.on('connection', (ws, req) => {
+  ws.on('error', console.error);
+
+  connectedClients.set(id, ws);
+  id = id + 1;
+
+  console.log('Connection Established');
+  //console.log(connectedClients);
+  //console.log(wss.clients);
+
+  ws.on('close', () => console.log('Client has disconnected!'));
+
+  ws.on('message', message => {
+    try {
+      const parsedMessage = JSON.parse(message);
+
+      if (parsedMessage.type === 'authorization') {
+        if (!token && token === '') {
+          ws.send(
+            JSON.stringify({
+              error: {
+                message: 'Unauthorized',
+                code: 401
+              }
+            })
+          );
+        }
+
+        ws.send(JSON.stringify({ type: 'authorization', status: 'success' }));
+
+        console.log(`Received message: ${message}`);
+        console.log('Parsed message:');
+        console.log(parsedMessage);
+      } else {
+        /*connectedClients.forEach((value, key) => {
+          if (value !== ws) {
+            value.send(`Message sent from client ${key}: ${message}`);
+          }
+        });*/
+      }
+    } catch (err) {
+      if (err.name === 'SyntaxError') {
+        ws.send(
+          JSON.stringify({
+            error: {
+              message: 'Not valid JSON',
+              code: 400
+            }
+          })
+        );
+      } else {
+        console.error(err);
+      }
+    }
+  });
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -45,6 +110,10 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/client', (req, res) => {
+  res.sendFile('/ws-client.html', { root: __dirname });
+});
+
 app.all('*', (req, res) => {
   res.status(404).json({
     error: {
@@ -54,6 +123,6 @@ app.all('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("server is running on port 3000");
 }); 
